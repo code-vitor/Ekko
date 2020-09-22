d8f08c41526d562b856658ab874b354015a42b04
Vitor Silva <vitorsesilva@gmail.com>
2020-09-21 21:04:56 -0300

    Div Box skins estilizada

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const { func } = require("prop-types")
+
+function changeImage01(){
+    let img = document.querySelector("#bg-image")
+
+    img.src = './images/Ekko.jpg'
+}
+
+function changeImage02(){
+    let img = document.querySelector("#bg-image")
+
+    img.src = './images/Ekko-True-Damage.jpg'
+}
+
+function changeImage03(){
+    let img = document.querySelector("#bg-image")
+
+    img.src = './images/Ekko-Pulsefire.jpg'
+}
+
+function changeImage04(){
+    let img = document.querySelector("#bg-image")
+
+    img.src = './images/Ekko-Halloween.jpg'
+}
+
+function changeImage05(){
+    let img = document.querySelector("#bg-image")
+
+    img.src = './images/Ekko-Storm.jpg'
+}
+function changeImage06(){
+    let img = document.querySelector("#bg-image")
+
+    img.src = './images/Ekko-school.jpg'
+}
+function changeImage07(){
+    let img = document.querySelector("#bg-image")
+
+    img.src = './images/ekko-project.jpg'
+}
